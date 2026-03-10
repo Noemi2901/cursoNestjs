df257dec5d2dc98a49fddac0b0fa457344d4006d
Noemi2901 <84550098+Noemi2901@users.noreply.github.com>
2026-03-10 15:04:50 -0400

    Refactor database connection settings for production
    Updated database connection configuration to use DATABASE_URL and added SSL support.

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,15 +7,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'musicdb',
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
+        type: 'postgres',
+        url: process.env.DATABASE_URL,
+        autoLoadEntities: true,
+        synchronize: true,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }),
     ArtistasModule,
     CancionesModule,
     AuthModule,
